29e79c58a931edbe83bc036f45f8275a5d8a2ec2
artemis <marlkiller@voidm.com>
2023-05-28 12:45:35 +0800

    Add redis

--- src/test/java/com/example/springbootlab/SpringbootLabApplicationTests.java
+++ src/test/java/com/example/springbootlab/SpringbootLabApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.springbootlab;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class SpringbootLabApplicationTests {
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @Test
-    void contextLoads() {
+    void test() {
+        stringRedisTemplate.opsForValue().set("k_dev", "v_dev");
+        System.out.println(stringRedisTemplate.opsForValue().get("k_dev"));
     }
-
 }
