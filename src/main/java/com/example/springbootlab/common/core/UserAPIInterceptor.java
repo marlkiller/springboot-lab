17a4044cf270840522a0876c668d0eb9870124f5
artemis <marlkiller@voidm.com>
2023-05-29 15:06:07 +0800

    Add user-api interceptors
    (cherry picked from commit 600739db311c7011452a5e5188a7cce68688a86c)

--- src/main/java/com/example/springbootlab/common/core/UserAPIInterceptor.java
+++ src/main/java/com/example/springbootlab/common/core/UserAPIInterceptor.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-public class GlobalInterceptor implements HandlerInterceptor {
+public class UserAPIInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(HandlerInterceptor.class);
 
@@ -16,25 +16,18 @@ public class GlobalInterceptor implements HandlerInterceptor {
         // 记录请求开始时间
         long startTime = System.currentTimeMillis();
         request.setAttribute("startTime", startTime);
-        // TODO global auth
-        // if ("anonymous".equals(request.getHeader("auth"))) {
-        //     throw new MyException("global permissions error");
-        // }
+        if (request.getHeader("user-api")==null) {
+            throw new MyException("user-api permissions error");
+        }
         // 返回 true 表示继续执行后续处理，返回 false 则终止请求处理
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        // 该方法在请求完成后执行
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        // 记录请求完成时间，并计算请求耗时
-        long endTime = System.currentTimeMillis();
-        long startTime = (Long) request.getAttribute("startTime");
-        long duration = endTime - startTime;
-        logger.info("Request completed. URL: {}, Time taken: {} ms", request.getRequestURI(), duration);
     }
 }
