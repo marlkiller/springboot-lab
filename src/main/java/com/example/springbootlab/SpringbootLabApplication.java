0471c9277550049d33ec639e2fed5dca5a008238
artemis <marlkiller@voidm.com>
2023-07-11 14:32:42 +0800

    一个基础的SpringBoot项目该包含哪些

--- src/main/java/com/example/springbootlab/SpringbootLabApplication.java
+++ src/main/java/com/example/springbootlab/SpringbootLabApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 一个基础的SpringBoot项目该包含哪些？
+// https://developer.aliyun.com/article/1247418?utm_content=g_1000374118#slide-36
 public class SpringbootLabApplication {
 
     public static void main(String[] args) {
